a8c5329e62f8e4dafa82ed3ea54bcd3ec716f63d
Patrick Leão <121454814+patleao83@users.noreply.github.com>
2025-08-28 20:57:29 -0300

    Update teste.java

--- teste/src/teste/teste.java
+++ teste/src/teste/teste.java
@@ -1,7 +1,7 @@
 package teste;
-
-public class teste {
-	public static void main(String[]args) {
-		System.out.print("Hello, World!");	}
-
+public class Teste {
+public static void main(String[] args) {
+int num1 = 3, num2 = 5, soma;
+soma = num1 + num2;
+}
 }
